5740b7cadf918e846209cc3ec4f0d620e0d4f14b
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-15 22:44:20 +0530

    Add the create user logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,16 @@
-const userService = require('')
+const userService = require('../services/user.service');
 
 exports.createUser = async (req, res, next) => {
   try {
+    // Get the username, password & confirm_password from the request body.
+    const {username, password, confirm_password} = req.body;
+
+    if(password.toLowerCase() !== confirm_password.toLowerCase()){
+      throw new Error(`The password & the confirm password fields do not match`);
+    }
+
     // Check if the user already exist.
+    userService.createUser()
     // Create the user in the redis.
   } catch (error) {
     console.error(`Error while getting users data`, err.message);
