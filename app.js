c5c54aac3263fd3a57761a47d2ab048e8b9fdb0e
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-15 20:44:26 +0530

    Add errorp handling middleware to the server

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 // Import the 3rd party packages
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 // Initialize the 3rd party packages
-bodyParser.urlencoded({extended : false});
+bodyParser.urlencoded({ extended: false });
 dotenv.config();
 
 // Create the server
 const server = express();
 
 // Make the server listen to the PORT(env file)
-server.listen(process.env.PORT);
+server.use((error, req, res, next) => {
+  // console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
+server.listen(process.env.PORT, () =>
+  console.log(`Server is listening for requests at port ${process.env.PORT}`)
+);
 
 /*
     TODO: Make the outlines of the routes
