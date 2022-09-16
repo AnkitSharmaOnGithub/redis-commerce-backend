3115d10bf93d10f30cc04ab5df851b3a1b844f37
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-16 20:12:41 +0530

    Integrated the controller in the router

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const userController = require('../controllers/user.controller');
+
 // Create user endpoint
-// router.post('', );
+router.post('/create', userController.createUser);
+
+module.exports = router;
