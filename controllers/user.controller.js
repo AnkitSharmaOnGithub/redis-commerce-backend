c405d701a200df2c6f1221f46dfa0f7daffedbdd
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-16 20:12:06 +0530

    Update the controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,17 +3,21 @@ const userService = require('../services/user.service');
 exports.createUser = async (req, res, next) => {
   try {
     // Get the username, password & confirm_password from the request body.
-    const {username, password, confirm_password} = req.body;
+    const {email, password, confirmPassword} = req.body;
+    // console.log(email, password, confirmPassword);
 
-    if(password.toLowerCase() !== confirm_password.toLowerCase()){
+    if(password.toLowerCase() !== confirmPassword.toLowerCase()){
       throw new Error(`The password & the confirm password fields do not match`);
     }
 
     // Check if the user already exist.
-    userService.createUser()
+    const data = await userService.createUser(email, password);
+    res.send({
+      'generatedKey' : data
+    });
     // Create the user in the redis.
-  } catch (error) {
-    console.error(`Error while getting users data`, err.message);
+  } catch (err) {
+    console.error(`Error while getting users data :-`, err.message);
     next(err);
   }
 };
