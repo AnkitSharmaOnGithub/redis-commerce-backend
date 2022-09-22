cf62b7ac7d620b0b5e40feea599a03c8ed340560
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-22 13:12:49 +0530

    Use hashedPassword to be stored in redis for more security

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,17 @@
 const userService = require("../services/user.service");
 
+// Add 3rd party packages
+const bcrypt = require("bcryptjs");
+
 exports.createUser = async (req, res, next) => {
   try {
     // Get the username, password & confirm_password from the request body.
-    const { email, password, confirmPassword } = req.body;
+    let { email, password, confirmPassword } = req.body;
 
     // Trim the whitespaces
     email = email.trim();
     password = password.trim();
     confirmPassword = confirmPassword.trim();
-
-    // console.log(email, password, confirmPassword);
 
     if (password.toLowerCase() !== confirmPassword.toLowerCase()) {
       throw new Error(
@@ -18,8 +19,11 @@ exports.createUser = async (req, res, next) => {
       );
     }
 
+    // Hash the password before storing it.
+    const hashedPassword = await bcrypt.hash(password, 12);
+
     // Check if the user already exist.
-    const data = await userService.createUser(email, password);
+    const data = await userService.createUser(email, hashedPassword);
     if (data.status) {
       res.status(200).send(data);
     } else {
@@ -35,18 +39,26 @@ exports.createUser = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     let { email, password } = req.body;
-    
+
     // Trim the whitespaces
     email = email.trim();
     password = password.trim();
 
-    let user_data = await userService.login(email,password);
-    
+    let user_data = await userService.login(email, password);
+
+    const password_matched = await bcrypt.compare(password, user_data.password);
+
+    if (password_matched === true) {
+      req.session.is_logged_in = true;
+      res.status(200).send({ status: "Logged in successfully" });
+    } else {
+      throw new Error("Incorrect user credentials entered.");
+    }
+
     // Check if the passwords match
 
     // Set the session
   } catch (error) {
     next(error);
   }
-
 };
