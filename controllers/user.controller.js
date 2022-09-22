e782e36ce41c0acc104a03017d72927e28794c6d
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-22 12:50:42 +0530

    Add string trim methods & login feature

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,12 @@ exports.createUser = async (req, res, next) => {
   try {
     // Get the username, password & confirm_password from the request body.
     const { email, password, confirmPassword } = req.body;
+
+    // Trim the whitespaces
+    email = email.trim();
+    password = password.trim();
+    confirmPassword = confirmPassword.trim();
+
     // console.log(email, password, confirmPassword);
 
     if (password.toLowerCase() !== confirmPassword.toLowerCase()) {
@@ -26,4 +32,21 @@ exports.createUser = async (req, res, next) => {
   }
 };
 
-exports.login = async (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+    
+    // Trim the whitespaces
+    email = email.trim();
+    password = password.trim();
+
+    let user_data = await userService.login(email,password);
+    
+    // Check if the passwords match
+
+    // Set the session
+  } catch (error) {
+    next(error);
+  }
+
+};
