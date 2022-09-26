5778ba6c44afd5975b69ad1a366640ef6f46122e
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-26 23:31:11 +0530

    Add auth helper for checking of logged in users

---
+++ helpers/auth.helper.js
@@ -0,0 +1,10 @@
+exports.isLoggedIn = (req, res, next) => {
+  console.log(req.url);
+
+  const excluded_urls = ["/user/create","/user/login"];
+
+  if (!excluded_urls.includes(req.url) && !req.session.isLoggedIn) {
+    throw new Error("Please login and the continue!");
+  }
+  next();
+};
