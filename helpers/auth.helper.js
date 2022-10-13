b8ff02d4247f180a51dea09f1f336f57ba98e4e8
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-13 22:34:33 +0530

    Add debug logs

--- helpers/auth.helper.js
+++ helpers/auth.helper.js
@@ -1,9 +1,13 @@
 exports.isLoggedIn = (req, res, next) => {
-  console.log(req.url);
+  // console.log(req.url);
+  console.log(req.session);
 
   const excluded_urls = ["/user/create", "/user/login"];
 
-  if (!excluded_urls.includes(req.url) && !req.session.isLoggedIn) {
+  // console.log(!excluded_urls.includes(req.url));
+  // console.log(!req.session.isLoggedIn);
+
+  if (!excluded_urls.includes(req.url) && req.session.isLoggedIn !== true) {
     throw new Error("Please login and the continue!");
   }
   next();
