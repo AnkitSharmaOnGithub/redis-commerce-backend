88dc15fd4da17136ac40d511efc55425356aa280
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-13 21:40:38 +0530

    Add authentication to route

--- routes/item.route.js
+++ routes/item.route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const itemController = require('../controllers/item.controller');
+const authHelper = require("../helpers/auth.helper");
 
 // Create item
-router.post('/create', itemController.createItem)
+router.post('/create', authHelper.isLoggedIn, itemController.createItem)
 
 // Get all items
 
