af4b0ff5024326149fc37c73b33b6e40a5cd715a
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-13 21:40:24 +0530

    Add test route

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user.controller');
+const authHelper = require("../helpers/auth.helper");
 
 // Create user endpoint
 router.post('/create', userController.createUser);
 
 router.post('/login', userController.login);
 
-router.get('/test', userController.test);
+router.get('/test', authHelper.isLoggedIn, userController.test);
 
 module.exports = router;
