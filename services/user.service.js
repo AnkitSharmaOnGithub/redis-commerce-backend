abba90194a6c7a7fe1dc4f12899b57e148120539
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-14 07:45:06 +0530

    Add user_id for each user to be saved in redis

--- services/user.service.js
+++ services/user.service.js
@@ -1,17 +1,19 @@
 const redisClient = require("../helpers/redis.helper");
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const keyHelper = require("../helpers/key.helper");
 
 exports.createUser = async (email, hashedPassword) => {
   try {
-    const redis_user_key = keyHelper.generateUserKey(email);
+    const user_id = uuidv4();
+    const redis_user_key = keyHelper.generateUserKey(user_id);
 
     // This can be imporved in future, via the response from redis.
-    const user_already_exists = await redisClient.hGet(redis_user_key, "email");
+    const user_already_exists = await redisClient.hGet(redis_user_key, id);
 
     if (!user_already_exists) {
       // If the user does not already exists, then save the user.
       const result = await Promise.all([
+        redisClient.hSet(redis_user_key,"id",user_id),
         redisClient.hSet(redis_user_key, "email", email),
         redisClient.hSet(redis_user_key, "password", hashedPassword),
       ]);
@@ -29,10 +31,10 @@ exports.createUser = async (email, hashedPassword) => {
 
 exports.login = async (email, password) => {
   try {
-    const redis_user_key = keyHelper.generateUserKey(email);
+    // const redis_user_key = keyHelper.generateUserKey(email);
 
     // Get the user data from the redis
-    const user_data = await redisClient.hGetAll(redis_user_key);
+    const user_data = await redisClient.hGet(redis_user_key);
     return user_data;
 
   } catch (error) {
