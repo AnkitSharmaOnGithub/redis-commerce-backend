57365ee41d8954ccad91a3528c2a18b270dbb969
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-18 23:50:49 +0530

    Update the middleware

--- helpers/auth.helper.js
+++ helpers/auth.helper.js
@@ -2,12 +2,10 @@
 const bcrypt = require("bcryptjs");
 
 exports.isLoggedIn = (req, res, next) => {
-  // console.log(req.url);
-  console.log(req.session);
 
   const excluded_urls = ["/user/create", "/user/login"];
 
-  if (!excluded_urls.includes(req.url) && req.session.isLoggedIn !== true) {
+  if (!excluded_urls.includes(req.url) && req.session.is_logged_in !== true) {
     throw new Error("Please login and the continue!");
   }
   next();
