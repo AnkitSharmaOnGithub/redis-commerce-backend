1d48953a2a45a656990495ef0b5c0f5ca83801d2
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-20 18:01:35 +0530

    Add create item feature

--- services/item.service.js
+++ services/item.service.js
@@ -1,7 +1,19 @@
 const redisClient = require("../helpers/redis.helper");
+const keyHelper = require("../helpers/key.helper");
+const uuidHelper = require("../helpers/uuid.helper");
 
 exports.createItem = async (itemData) => {
+    const item_id = uuidHelper.generateUniqueUuidKey();
+    const item_key = keyHelper.generateItemKey(item_id);
+
+    // Store the item Data into a hash
     await Promise.all([
-        
-    ])
+        redisClient.hSet(item_key,"name", itemData.name),
+        redisClient.hSet(item_key,"desc", itemData.desc),
+        redisClient.hSet(item_key,"price", itemData.price),
+        redisClient.hSet(item_key,"valid_till", itemData.valid_till)
+    ]);
+
+    // Map the user to the item
+    
 }
