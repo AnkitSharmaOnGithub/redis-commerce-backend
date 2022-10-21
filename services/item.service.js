25a6068ab1adff8587724f20ef58547e981f45fb
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-21 20:03:08 +0530

    Add createItem & getItem functionality

--- services/item.service.js
+++ services/item.service.js
@@ -3,26 +3,33 @@ const keyHelper = require("../helpers/key.helper");
 const uuidHelper = require("../helpers/uuid.helper");
 
 exports.createItem = async (itemData) => {
+  try {
     const item_id = uuidHelper.generateUniqueUuidKey();
     const item_key = keyHelper.generateItemKey(item_id);
 
     // Store the item Data into a hash
-    await Promise.all([
-        redisClient.hSet(item_key,"name", itemData.name),
-        redisClient.hSet(item_key,"desc", itemData.desc),
-        redisClient.hSet(item_key,"price", itemData.price),
-        redisClient.hSet(item_key,"valid_till", itemData.valid_till)
+    const creation_status = await Promise.all([
+      redisClient.hSet(item_key, "name", itemData.name),
+      redisClient.hSet(item_key, "desc", itemData.desc),
+      redisClient.hSet(item_key, "price", itemData.price),
+      redisClient.hSet(item_key, "valid_till", itemData.valid_till),
     ]);
 
+    if (creation_status.every((el) => el === 1)) {
+      return true;
+    }
+
     // Map the user to the item
-    
+  } catch (error) {
+    return error;
+  }
 }
 
 exports.getItem = async (itemId) => {
   try {
     const itemKey = keyHelper.generateItemKey(itemId);
 
-    const item = await redisClient.hGet(itemKey);
+    const item = await redisClient.hGetAll(itemKey);
     return item;
   } catch (error) {
     return error;
