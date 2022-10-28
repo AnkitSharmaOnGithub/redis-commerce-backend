628e7d849c11761f47cf60f22ddb9b84b814343d
Ankit Sharma <ankitsharma101299@gmail.com>
2022-10-28 22:38:54 +0530

    Add new key helper methods

--- helpers/key.helper.js
+++ helpers/key.helper.js
@@ -3,3 +3,7 @@ exports.generateUserKey = (id) => `users#${id}`;
 exports.generateSessionKey = (id) => `session#${id}`;
 
 exports.generateItemKey = (id) => `item#${id}`;
+
+exports.generateUniqueUserKey = () => `usernames`;
+
+exports.generateItemLikeKey = (userId) => `items#like-${userId}`;
