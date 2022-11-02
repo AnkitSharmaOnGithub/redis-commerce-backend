f17a06f60661e8d92567b26793465134887f7542
Ankit Sharma <ankitsharma101299@gmail.com>
2022-11-02 19:48:12 +0530

    Refactored validations

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -4,11 +4,11 @@ exports.createItem = async (req, res, next) => {
   try {
     // console.log(req.session);
 
-    // const keys = ["name", "desc", "price", "valid_till"];
+    const keys = ["name", "desc", "price", "valid_till"];
     
     // Trim the fields & do validations
-    for (let key in req.body) {
-      if (!req.body[key].trim()) {
+    for (let key of keys) {
+      if (!req.body[key] || !req.body[key].trim()) {
         throw new Error(`${key} cannot be empty`);
       }
 
